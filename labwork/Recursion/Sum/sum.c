4daf69c4e5bbf907c307d927aa6bae3682d2391d
MD Mojammel Haque Shanto <mojammelhaqueshanto29@gmail.com>
2026-06-27 18:31:47 +0600

    Implement recursive sum function in C

--- labwork/Recursion/Sum/sum.c
+++ labwork/Recursion/Sum/sum.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+int sum(int arr[], int n)
+{
+    if (n <= 0)
+        return 0;
+    return arr[n - 1] + sum(arr, n - 1);
+}
+int main()
+{
+    int n;
+    printf("Enter size:\n");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    int result = sum(arr, n);
+    printf("Sum=%d", result);
+    return 0;
+}
